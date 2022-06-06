695922c2253965902d14bfc0d01f147ed1374641
Aleh Dumava <odumava@mail.ru>
2022-06-06 21:07:55 +0300

    Refactor the class ParsingBean

---
+++ src/main/java/com/example/app/ParsingBean.java
@@ -0,0 +1,20 @@
+package com.example.app;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+
+@ManagedBean(name = "obj")
+@SessionScoped
+public class ParsingBean implements Serializable {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
